8ab38cdf70dd347994daf85559725aeac0dfdb4e
Joe Myers <joecaricature@gmail.com>
2021-06-08 19:58:46 -0400

    implemented hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,78 @@
+// @ts-ignore
 import React from 'react'
+import { useState } from 'react'
 import { Slider } from './components/Slider.jsx'
 import { ColorSwatch } from './components/ColorSwatch'
 import { Hsla } from './components/hsla'
 
-export class App extends React.Component {
-  state = {
-    h: Math.floor(Math.random() * 360),
-    s: Math.floor(Math.random() * 100),
-    l: Math.floor(Math.random() * 100),
+export function App() {
+  const [hue, setHue] = useState(Math.floor(Math.random() * 360))
+  const [saturation, setSaturation] = useState(Math.floor(Math.random() * 100))
+  const [light, setLight] = useState(Math.floor(Math.random() * 100))
+
+  const newBackgroundColor = `hsl(${hue},${saturation}%,${light}%)`
+  const newStyle = { backgroundColor: newBackgroundColor }
+
+  function handleHueChange(event) {
+    setHue(event.target.value)
   }
 
-  handleHueChange = (event) => {
-    this.setState({ h: event.target.value })
+  function handleSatChange(event) {
+    setSaturation(event.target.value)
   }
 
-  handleSatChange = (event) => {
-    this.setState({ s: event.target.value })
+  function handleLightChange(event) {
+    setLight(event.target.value)
   }
 
-  handleLightChange = (event) => {
-    this.setState({ l: event.target.value })
+  function randomColor() {
+    setHue(Math.floor(Math.random() * 360))
+    setSaturation(Math.floor(Math.random() * 100))
+    setLight(Math.floor(Math.random() * 100))
   }
 
-  randomColor = () => {
-    this.setState({
-      h: Math.floor(Math.random() * 360),
-      s: Math.floor(Math.random() * 100),
-      l: Math.floor(Math.random() * 100),
-    })
-  }
-
-  render() {
-    const newBackgroundColor = `hsl(${this.state.h},${this.state.s}%,${this.state.l}%)`
-    const newStyle = { backgroundColor: newBackgroundColor }
-
-    return (
-      <>
-        <h1>Classy Color Picker</h1>
-        <div className="Picker">
-          <div className="Color">
-            <div className="ColorTitle">Color</div>
-            <ColorSwatch style={newStyle}></ColorSwatch>
-            <Hsla color={newBackgroundColor}></Hsla>
-          </div>
-          <div className="Sliders">
-            <label> H</label>
-            <Slider
-              type="range"
-              id="h"
-              className="Hue"
-              value={this.state.h}
-              max="360"
-              onChange={this.handleHueChange}
-            />
-            <label>S</label>
-            <Slider
-              type="range"
-              id="s"
-              className="Saturation"
-              value={this.state.s}
-              max="100"
-              onChange={this.handleSatChange}
-            />
-            <label>L</label>
-            <Slider
-              type="range"
-              id="l"
-              className="Light"
-              value={this.state.l}
-              max="100"
-              onChange={this.handleLightChange}
-            />
-          </div>
+  return (
+    <>
+      <h1>Functional Color Picker!</h1>
+      <div className="Picker">
+        <div className="Color">
+          <div className="ColorTitle">Color</div>
+          <ColorSwatch style={newStyle}></ColorSwatch>
+          <Hsla color={newBackgroundColor}></Hsla>
         </div>
-        <button className="RandomColor" onClick={this.randomColor}>
-          Random Color!
-        </button>
-      </>
-    )
-  }
+        <div className="Sliders">
+          <label> H</label>
+          <Slider
+            type="range"
+            id="h"
+            className="Hue"
+            value={hue}
+            max="360"
+            onChange={handleHueChange}
+          />
+          <label>S</label>
+          <Slider
+            type="range"
+            id="s"
+            className="Saturation"
+            value={saturation}
+            max="100"
+            onChange={handleSatChange}
+          />
+          <label>L</label>
+          <Slider
+            type="range"
+            id="l"
+            className="Light"
+            value={light}
+            max="100"
+            onChange={handleLightChange}
+          />
+        </div>
+      </div>
+      <button className="RandomColor" onClick={randomColor}>
+        Random Color!
+      </button>
+    </>
+  )
 }
